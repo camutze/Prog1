a526d3ce629da086d27b815b077e03684d07c03b
camutze <carlosmutzenberg@ufpr.br>
2023-05-24 14:47:30 -0300

    Inclusao t1 e organização das pastas

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Prog1
 Vou deixar neste repositorios todos os trabalhos que serão feitos na matéria de Programação 1
+#LEIA
+Disponibilizo este código como uma fonte de consulta para auxiliar você em suas necessidades de programação. Elaborei-o com o intuito de fornecer exemplos e orientações gerais sobre o assunto em questão. No entanto, é importante salientar que a cópia direta deste código não é recomendada.
+
+Ao utilizar este código como referência, recomendo que você o utilize como ponto de partida para suas próprias implementações. Isso garantirá que você desenvolva um entendimento mais profundo dos conceitos abordados e adquira habilidades práticas na programação.
+
+É fundamental que você compreenda cada linha de código, adapte-o às suas necessidades específicas e o personalize de acordo com os requisitos do seu projeto. A cópia direta deste código sem uma compreensão completa de seu funcionamento pode resultar em erros ou resultados indesejados.
+
+Além disso, lembre-se de considerar as melhores práticas de programação, como a organização e a legibilidade do código, o uso adequado de comentários e a aplicação de boas técnicas de programação. Isso ajudará você a escrever um código mais eficiente, sustentável e de fácil manutenção.
+#Responsabilidade
+Portanto, encorajo você a utilizar este código como uma referência valiosa, mas sempre adaptando-o e complementando-o com seu próprio conhecimento e criatividade. A prática da programação é um processo contínuo de aprendizado e aprimoramento, e construir soluções originais é uma habilidade essencial para o sucesso na área de desenvolvimento de software
--- tp1/tp1/libAgenda.h
+++ tp1/tp1/libAgenda.h
@@ -2,16 +2,16 @@
 #define HORAS_DO_DIA 24
 #define DIAS_DO_ANO 365
 
-/* estrutura para representar uma data, composta por dia, mes e ano */
+/* estrutura para representar uma data, composta por dia, mês e ano */
 struct data {
 	int dia;
 	int mes;
 	int ano;
 };
 
-/* estrutura para representar um compromisso, isto eh, uma data e uma hora */
+/* estrutura para representar um compromisso, isto é, uma data e uma hora */
 struct compromisso {
-	struct data data_compr;	/* dia, mes, ano */
+	struct data data_compr;	/* dia, mês, ano */
 	int hora_compr; /* hora do compromisso entre 0 e 23 */
 };
 
@@ -23,37 +23,40 @@ struct dias {
 /* estrutura para representar uma agenda de um dado ano, com 365 dias de 24 horas */
 struct agenda {
 	int ano;
-	struct dias agenda_do_ano[DIAS_DO_ANO]; /* vetor agenda do ano, nao considerar ano bissexto */
+	struct dias agenda_do_ano[DIAS_DO_ANO]; /* vetor agenda do ano, a ser modificado em ano bissexto */
 };
 
-/* funcao com implementacao dada pelos professores para calculo do dia do 
- * ano, que servira de indice para o vetor da agenda */
+/* função com implementação dada pelos professores para cálculo do dia do 
+ * ano, que servirá de índice para o vetor da agenda */
 int obtemDiaDoAno(struct data d);
 
 /* inicializa a agenda do ano corrente, onde cada hora de cada dia deve ter o 
- * valor 0 para indicar que nao ha um compromisso marcado. Retorna uma agenda
+ * valor 0 para indicar que não há um compromisso marcado. Retorna uma agenda
  * livre */ 
 struct agenda criaAgenda(int ano);
 
-/* le um compromisso passado pelo usuario, com data completa e hora pretendida. 
- * Cabe ao usuario desta funcao validar a data e hora do compromisso */
+/* lê um compromisso passado pelo usuário, com data completa e hora pretendida. 
+ * O compromisso deve ser validado, isto é, a data deve estar correta, 
+ * considerando os dias de cada mês e o ano da agenda criada, assim como a  
+ * a hora, que deve ser entre 0 e 23, sem frações. 
+ * Retorna um compromisso válido. */
 struct compromisso leCompromisso();
 
 /* dado um compromisso, retorna a hora definida */
 int obtemHora(struct compromisso compr);
 
-/* retorna o ano atribuido a uma agenda criada */
+/* retorna o ano atribuído a uma agenda criada */
 int obtemAno(struct agenda ag);
 
-/* Valida uma data lida do usuario; 
- * Retorna 1 se a data for valida e 0 caso contrario */
+/* Valida uma data lida do usuário; utilizar como função auxiliar da função
+ * leCompromisso(). Retorna 0 se a data for inválida */
 int validaData(struct data d, struct agenda ag);
 
-/* Retorna 0 se data e horario já estiverem ocupados, 1 se data e horario
+/* Retorna 0 se data e horário já estiverem ocupados, 1 se data e horário
  * estiverem livres */
 int verificaDisponibilidade(struct compromisso compr, struct agenda ag);
 
-/* Dada uma agenda e um compromisso valido, isto eh, com data/hora validos, 
+/* Dada uma agenda e um compromisso válido, isto é, com data/hora válidos, 
  * hora livre e dentro do ano da agenda, muda o valor da hora do compromisso
  * de 0 (livre) para 1 (ocupado). Retorna a nova agenda com o compromisso
  * marcado. */
