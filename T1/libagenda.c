64a4c63d522da8e2a6c0c6e63689ae7c9dc56430
Carlos Mutzenberg <catm21@inf.ufpr.br>
2023-06-01 21:42:00 -0300

    joao

--- T1/libagenda.c
+++ T1/libagenda.c
@@ -1,301 +1,253 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "libagenda.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 agenda_t* cria_agenda() {
-	agenda_t* agenda = (agenda_t*)malloc(sizeof(agenda_t));
-	if (agenda == NULL) {
-		return NULL;
-	}
-
-	agenda->ptr_mes_atual = NULL;
-	agenda->mes_atual = 1;
-
-	return agenda;
+    agenda_t* nova_agenda = (agenda_t*)malloc(sizeof(agenda_t));
+    nova_agenda->mes_atual = 1;
+    nova_agenda->ptr_mes_atual = NULL;
+    return nova_agenda;
 }
 
 compromisso_t* cria_compromisso(horario_compromisso_t hc, int id, char* descricao) {
-	compromisso_t* compromisso = (compromisso_t*)malloc(sizeof(compromisso_t));
-	if (compromisso == NULL) {
-		return NULL;
-	}
-
-	compromisso->id = id;
-	compromisso->descricao = strdup(descricao);
-	compromisso->inicio = hc.ini_h * 60 + hc.ini_m;
-	compromisso->fim = hc.fim_h * 60 + hc.fim_m;
-	compromisso->prox = NULL;
-
-	return compromisso;
+    compromisso_t* novo_compromisso = (compromisso_t*)malloc(sizeof(compromisso_t));
+    novo_compromisso->id = id;
+    novo_compromisso->descricao = descricao;
+    novo_compromisso->inicio = hc.ini_h * 60 + hc.ini_m;
+    novo_compromisso->fim = hc.fim_h * 60 + hc.fim_m;
+    novo_compromisso->prox = NULL;
+    return novo_compromisso;
 }
 
 void destroi_agenda(agenda_t* agenda) {
-	if (agenda == NULL) {
-		return;
-	}
-
-	mes_t* mes_atual = agenda->ptr_mes_atual;
-	while (mes_atual != NULL) {
-		dia_t* dia_atual = mes_atual->dias;
-		while (dia_atual != NULL) {
-			compromisso_t* compromisso_atual = dia_atual->comprs;
-			while (compromisso_atual != NULL) {
-				compromisso_t* prox_compromisso = compromisso_atual->prox;
-				free(compromisso_atual->descricao);
-				free(compromisso_atual);
-				compromisso_atual = prox_compromisso;
-			}
-			dia_t* prox_dia = dia_atual->prox;
-			free(dia_atual);
-			dia_atual = prox_dia;
-		}
-		mes_t* prox_mes = mes_atual->prox;
-		free(mes_atual);
-		mes_atual = prox_mes;
-	}
-
-	free(agenda);
+    if (agenda == NULL)
+        return;
+    
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL) {
+        dia_t* dia_atual = mes_atual->dias;
+        while (dia_atual != NULL) {
+            compromisso_t* compr_atual = dia_atual->comprs;
+            while (compr_atual != NULL) {
+                compromisso_t* prox_compromisso = compr_atual->prox;
+                free(compr_atual->descricao);
+                free(compr_atual);
+                compr_atual = prox_compromisso;
+            }
+            dia_t* prox_dia = dia_atual->prox;
+            free(dia_atual);
+            dia_atual = prox_dia;
+        }
+        mes_t* prox_mes = mes_atual->prox;
+        free(mes_atual);
+        mes_atual = prox_mes;
+    }
+    free(agenda);
 }
 
 int marca_compromisso_agenda(agenda_t* agenda, int dia, compromisso_t* compr) {
-	if (agenda == NULL || compr == NULL) {
-		return 0;
-	}
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL && mes_atual->mes != agenda->mes_atual)
+        mes_atual = mes_atual->prox;
 
-	mes_t* mes_atual = agenda->ptr_mes_atual;
-	while (mes_atual != NULL) {
-		if (mes_atual->mes == agenda->mes_atual) {
-			dia_t* dia_atual = mes_atual->dias;
-			while (dia_atual != NULL) {
-				if (dia_atual->dia == dia) {
-					compromisso_t* compromisso_anterior = NULL;
-					compromisso_t* compromisso_atual = dia_atual->comprs;
+    if (mes_atual == NULL)
+        return 0; // Mês atual não encontrado na lista de meses
 
-					while (compromisso_atual != NULL && compr->inicio > compromisso_atual->inicio) {
-						compromisso_anterior = compromisso_atual;
-						compromisso_atual = compromisso_atual->prox;
-					}
+    dia_t* dia_atual = mes_atual->dias;
+    while (dia_atual != NULL && dia_atual->dia != dia)
+        dia_atual = dia_atual->prox;
 
-					if (compromisso_atual != NULL && compr->fim > compromisso_atual->inicio) {
-						return -1; // Compromisso tem interseção com outro
-					}
+    if (dia_atual == NULL)
+        return 0; // Dia não encontrado na lista de dias
 
-					if (compromisso_anterior != NULL) {
-						compromisso_anterior->prox = compr;
-					} else {
-						dia_atual->comprs = compr;
-					}
+    if (dia_atual->comprs == NULL) {
+        dia_atual->comprs = compr;
+        return 1; // Sucesso ao marcar o compromisso (lista vazia)
+    }
 
-					compr->prox = compromisso_atual;
-					return 1; // Sucesso
-				}
-				dia_atual = dia_atual->prox;
-			}
-			break;
-		}
-		mes_atual = mes_atual->prox;
-	}
+    compromisso_t* anterior = NULL;
+    compromisso_t* atual = dia_atual->comprs;
+    while (atual != NULL) {
+        if (compr->fim <= atual->inicio) {
+            compr->prox = atual;
+            if (anterior != NULL)
+                anterior->prox = compr;
+            else
+                dia_atual->comprs = compr;
+            return 1; // Sucesso ao marcar o compromisso (interseção à esquerda ou início igual)
+        } else if (compr->inicio >= atual->fim) {
+            anterior = atual;
+            atual = atual->prox;
+        } else {
+            return -1; // Interseção com outro compromisso
+        }
+    }
 
-	return 0;
+    anterior->prox = compr;
+    return 1; // Sucesso ao marcar o compromisso (interseção à direita)
 }
 
 int desmarca_compromisso_agenda(agenda_t* agenda, int dia, compromisso_t* compr) {
-	if (agenda == NULL || compr == NULL) {
-		return 0;
-	}
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL && mes_atual->mes != agenda->mes_atual)
+        mes_atual = mes_atual->prox;
 
-	mes_t* mes_atual = agenda->ptr_mes_atual;
-	while (mes_atual != NULL) {
-		if (mes_atual->mes == agenda->mes_atual) {
-			dia_t* dia_atual = mes_atual->dias;
-			while (dia_atual != NULL) {
-				if (dia_atual->dia == dia) {
-					compromisso_t* compromisso_anterior = NULL;
-					compromisso_t* compromisso_atual = dia_atual->comprs;
+    if (mes_atual == NULL)
+        return 0; // Mês atual não encontrado na lista de meses
 
-					while (compromisso_atual != NULL && compromisso_atual != compr) {
-						compromisso_anterior = compromisso_atual;
-						compromisso_atual = compromisso_atual->prox;
-					}
+    dia_t* dia_atual = mes_atual->dias;
+    while (dia_atual != NULL && dia_atual->dia != dia)
+        dia_atual = dia_atual->prox;
 
-					if (compromisso_atual != NULL) {
-						if (compromisso_anterior != NULL) {
-							compromisso_anterior->prox = compromisso_atual->prox;
-						} else {
-							dia_atual->comprs = compromisso_atual->prox;
-						}
+    if (dia_atual == NULL)
+        return 0; // Dia não encontrado na lista de dias
 
-						free(compromisso_atual->descricao);
-						free(compromisso_atual);
-						return 1; // Sucesso
-					}
-				}
-				dia_atual = dia_atual->prox;
-			}
-			break;
-		}
-		mes_atual = mes_atual->prox;
-	}
+    compromisso_t* anterior = NULL;
+    compromisso_t* atual = dia_atual->comprs;
+    while (atual != NULL) {
+        if (atual == compr) {
+            if (anterior != NULL)
+                anterior->prox = atual->prox;
+            else
+                dia_atual->comprs = atual->prox;
+            free(atual->descricao);
+            free(atual);
+            return 1; // Sucesso ao desmarcar o compromisso
+        }
+        anterior = atual;
+        atual = atual->prox;
+    }
 
-	return 0;
+    return 0; // Compromisso não encontrado
 }
 
 void imprime_agenda_mes(agenda_t* agenda) {
-	if (agenda == NULL) {
-		return;
-	}
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL && mes_atual->mes != agenda->mes_atual)
+        mes_atual = mes_atual->prox;
 
-	mes_t* mes_atual = agenda->ptr_mes_atual;
-	while (mes_atual != NULL) {
-		if (mes_atual->mes == agenda->mes_atual) {
-			dia_t* dia_atual = mes_atual->dias;
-			while (dia_atual != NULL) {
-				compromisso_t* compromisso_atual = dia_atual->comprs;
-				while (compromisso_atual != NULL) {
-					horario_compromisso_t hc = hc_compr(compromisso_atual);
-					printf("Dia: %d, Hora de Início: %d:%02d, Hora de Fim: %d:%02d, ID: %d, Descrição: %s\n",
-						   dia_atual->dia, hc.ini_h, hc.ini_m, hc.fim_h, hc.fim_m, id_compr(compromisso_atual),
-						   descricao_compr(compromisso_atual));
-					compromisso_atual = compromisso_atual->prox;
-				}
-				dia_atual = dia_atual->prox;
-			}
-			break;
-		}
-		mes_atual = mes_atual->prox;
-	}
+    if (mes_atual == NULL) {
+        printf("Mês atual não encontrado na lista de meses.\n");
+        return;
+    }
+
+    dia_t* dia_atual = mes_atual->dias;
+    while (dia_atual != NULL) {
+        compromisso_t* compr_atual = dia_atual->comprs;
+        while (compr_atual != NULL) {
+            horario_compromisso_t hc = hc_compr(compr_atual);
+            printf("Dia: %d, Início: %02d:%02d, Fim: %02d:%02d, ID: %d, Descrição: %s\n",
+                   dia_atual->dia, hc.ini_h, hc.ini_m, hc.fim_h, hc.fim_m,
+                   id_compr(compr_atual), descricao_compr(compr_atual));
+            compr_atual = prox_compr(compr_atual);
+        }
+        dia_atual = dia_atual->prox;
+    }
 }
 
 int mes_atual_agenda(agenda_t* agenda) {
-	if (agenda == NULL) {
-		return 0;
-	}
-
-	return agenda->mes_atual;
+    return agenda->mes_atual;
 }
 
 void prim_mes_agenda(agenda_t* agenda) {
-	if (agenda == NULL) {
-		return;
-	}
-
-	mes_t* mes_atual = agenda->ptr_mes_atual;
-	while (mes_atual != NULL && mes_atual->mes != 1) {
-		mes_atual = mes_atual->ant;
-	}
-
-	agenda->ptr_mes_atual = mes_atual;
-	agenda->mes_atual = 1;
+    agenda->mes_atual = 1;
+    agenda->ptr_mes_atual = NULL;
 }
 
 int prox_mes_agenda(agenda_t* agenda) {
-	if (agenda == NULL) {
-		return 0;
-	}
+    if (agenda->mes_atual == 12)
+        return 0; // Não é possível avançar além do mês 12
 
-	mes_t* mes_atual = agenda->ptr_mes_atual;
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL && mes_atual->mes != agenda->mes_atual)
+        mes_atual = mes_atual->prox;
 
-	if (mes_atual == NULL) {
-		mes_atual = (mes_t*)malloc(sizeof(mes_t));
-		if (mes_atual == NULL) {
-			return 0;
-		}
+    if (mes_atual != NULL) {
+        agenda->mes_atual++;
+        agenda->ptr_mes_atual = mes_atual->prox;
+        return agenda->mes_atual;
+    } else {
+        mes_t* novo_mes = (mes_t*)malloc(sizeof(mes_t));
+        novo_mes->mes = agenda->mes_atual + 1;
+        novo_mes->dias = NULL;
+        novo_mes->prox = NULL;
 
-		mes_atual->mes = 1;
-		mes_atual->dias = NULL;
-		mes_atual->prox = mes_atual;
-		mes_atual->ant = mes_atual;
-		agenda->ptr_mes_atual = mes_atual;
-		agenda->mes_atual = 1;
-		return 1;
-	}
+        if (agenda->ptr_mes_atual == NULL) {
+            agenda->ptr_mes_atual = novo_mes;
+        } else {
+            mes_t* temp = agenda->ptr_mes_atual;
+            while (temp->prox != NULL)
+                temp = temp->prox;
+            temp->prox = novo_mes;
+            novo_mes->ant = temp;
+        }
 
-	if (mes_atual->prox == agenda->ptr_mes_atual) {
-		mes_t* novo_mes = (mes_t*)malloc(sizeof(mes_t));
-		if (novo_mes == NULL) {
-			return 0;
-		}
-
-		novo_mes->mes = mes_atual->mes + 1;
-		novo_mes->dias = NULL;
-		novo_mes->prox = mes_atual->prox;
-		novo_mes->ant = mes_atual;
-		mes_atual->prox->ant = novo_mes;
-		mes_atual->prox = novo_mes;
-		agenda->ptr_mes_atual = novo_mes;
-		agenda->mes_atual = novo_mes->mes;
-		return novo_mes->mes;
-	}
-
-	agenda->ptr_mes_atual = mes_atual->prox;
-	agenda->mes_atual = mes_atual->prox->mes;
-	return mes_atual->prox->mes;
+        agenda->mes_atual++;
+        agenda->ptr_mes_atual = novo_mes;
+        return agenda->mes_atual;
+    }
 }
 
 int ant_mes_agenda(agenda_t* agenda) {
-	if (agenda == NULL) {
-		return 0;
-	}
+    if (agenda->mes_atual == 1)
+        return 0; // Não é possível retroceder antes do mês 1
 
-	mes_t* mes_atual = agenda->ptr_mes_atual;
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL && mes_atual->mes != agenda->mes_atual)
+        mes_atual = mes_atual->prox;
 
-	if (mes_atual == NULL) {
-		mes_atual = (mes_t*)malloc(sizeof(mes_t));
-		if (mes_atual == NULL) {
-			return 0;
-		}
+    if (mes_atual != NULL) {
+        agenda->mes_atual--;
+        agenda->ptr_mes_atual = mes_atual->ant;
+        return agenda->mes_atual;
+    } else {
+        mes_t* novo_mes = (mes_t*)malloc(sizeof(mes_t));
+        novo_mes->mes = agenda->mes_atual - 1;
+        novo_mes->dias = NULL;
+        novo_mes->prox = agenda->ptr_mes_atual;
+        novo_mes->ant = NULL;
 
-		mes_atual->mes = 1;
-		mes_atual->dias = NULL;
-		mes_atual->prox = mes_atual;
-		mes_atual->ant = mes_atual;
-		agenda->ptr_mes_atual = mes_atual;
-		agenda->mes_atual = 1;
-		return 1;
-	}
-
-	if (mes_atual->ant == agenda->ptr_mes_atual) {
-		mes_t* novo_mes = (mes_t*)malloc(sizeof(mes_t));
-		if (novo_mes == NULL) {
-			return 0;
-		}
-
-		novo_mes->mes = mes_atual->mes - 1;
-		novo_mes->dias = NULL;
-		novo_mes->prox = mes_atual;
-		novo_mes->ant = mes_atual->ant;
-		mes_atual->ant->prox = novo_mes;
-		mes_atual->ant = novo_mes;
-		agenda->ptr_mes_atual = novo_mes;
-		agenda->mes_atual = novo_mes->mes;
-		return novo_mes->mes;
-	}
-
-	agenda->ptr_mes_atual = mes_atual->ant;
-	agenda->mes_atual = mes_atual->ant->mes;
-	return mes_atual->ant->mes;
+        agenda->mes_atual--;
+        agenda->ptr_mes_atual = novo_mes;
+        return agenda->mes_atual;
+    }
 }
 
 compromisso_t* compr_agenda(agenda_t* agenda, int dia) {
-	if (agenda == NULL) {
-		return NULL;
-	}
+    mes_t* mes_atual = agenda->ptr_mes_atual;
+    while (mes_atual != NULL && mes_atual->mes != agenda->mes_atual)
+        mes_atual = mes_atual->prox;
 
-	mes_t* mes_atual = agenda->ptr_mes_atual;
-	while (mes_atual != NULL) {
-		if (mes_atual->mes == agenda->mes_atual) {
-			dia_t* dia_atual = mes_atual->dias;
-			while (dia_atual != NULL) {
-				if (dia_atual->dia == dia) {
-					return dia_atual->comprs;
-				}
-				dia_atual = dia_atual->prox;
-			}
-			break;
-		}
-		mes_atual = mes_atual->prox;
-	}
+    if (mes_atual == NULL)
+        return NULL; // Mês atual não encontrado na lista de meses
 
-	return NULL;
+    dia_t* dia_atual = mes_atual->dias;
+    while (dia_atual != NULL && dia_atual->dia != dia)
+        dia_atual = dia_atual->prox;
+
+    if (dia_atual == NULL)
+        return NULL; // Dia não encontrado na lista de dias
+
+    return dia_atual->comprs;
+}
+
+compromisso_t* prox_compr(compromisso_t* compr) {
+    return compr->prox;
+}
+
+horario_compromisso_t hc_compr(compromisso_t* compr) {
+    horario_compromisso_t hc;
+    hc.ini_h = compr->inicio / 60;
+    hc.ini_m = compr->inicio % 60;
+    hc.fim_h = compr->fim / 60;
+    hc.fim_m = compr->fim % 60;
+    return hc;
+}
+
+int id_compr(compromisso_t* compr) {
+    return compr->id;
+}
+
+char* descricao_compr(compromisso_t* compr) {
+    return compr->descricao;
 }
