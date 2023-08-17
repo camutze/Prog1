ae2ff707bccec0abc07d9925d2c2e94628900692
camutze <carlosmutzenberg@ufpr.br>
2023-08-17 16:56:42 -0300

    tp1 comece

---
+++ tp1/racionais.c
@@ -0,0 +1,46 @@
+#include "racionais.h"
+/* acrescente demais includes que voce queira ou precise */
+
+/* 
+ * Implemente aqui as funcoes definidas no racionais.h 
+ * caso queira, você pode definir neste arquivo funcoes
+ * adicionais que serao internas e so podem ser usadas
+ * aqui.
+*/
+
+int aleat(int min, int max) {
+}
+
+int mdc(int a, int b) {
+}
+
+int mmc(int a, int b) {
+}
+
+struct racional simplifica_r(struct racional r) {
+}
+
+struct racional cria_r(int numerador, int denominador) {
+}
+
+struct racional sorteia_r(int n) {
+}
+
+void imprime_r(struct racional r) {
+}
+
+int valido_r(struct racional r) {
+}
+
+struct racional soma_r(struct racional r1, struct racional r2) {
+}
+
+struct racional subtrai_r(struct racional r1, struct racional r2) {
+}
+
+struct racional multiplica_r(struct racional r1, struct racional r2) {
+}
+
+struct racional divide_r(struct racional r1, struct racional r2) {
+}
+
