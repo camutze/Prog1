94fe204e9d5f1e201870541bbb01782e0a241442
camutze <carlosmutzenberg@ufpr.br>
2023-08-31 11:59:41 -0300

    tp2 na mira

--- tp2/racionais.c
+++ tp2/racionais.c
@@ -1,31 +1,213 @@
-/* acrescente demais includes que voce queira ou precise */
+#include <stdio.h>
+#include <stdlib.h>
 #include "racionais.h"
 
-/* 
- * Implemente neste arquivo as funções definidas no racionais.h.
- * Caso queira, você pode definir neste arquivo funções adicionais
- * que serão internas a este arquivo.
- *
- * Por exemplo, as funções aleat, mdc e mmc devem ser
- * implementadas neste arquivo.
-*/
-
 /* retorna um numero aleatorio entre min e max, inclusive. */
-int aleat (int min, int max){
-/* coloque o codigo aqui */
+int aleat(int min, int max)
+{
+    /*garantindo que max seja maior*/
+    int troca = max;
+    max = min;
+    min = troca;
+
+    return min + (rand() % (max - min + 1));
 }
 
 /* Maximo Divisor Comum entre a e b      */
 /* calcula o mdc pelo metodo de Euclides */
-int mdc (int a, int b){
-/* coloque o codigo aqui */
+int mdc(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return mdc(b, a % b);
 }
 
 /* Minimo Multiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
-int mmc (int a, int b){
-/* coloque o codigo aqui */
+int mmc(int a, int b)
+{
+    return ((a * b) / mdc(a, b));
 }
 
-/* implemente aqui as funções declaradas em racionais.h */
+struct racional cria_r(int numerador, int denominador)
+{
+    struct racional novo;
+    novo.num = numerador;
+    novo.den = denominador;
 
+    novo.valido = valido_r(novo);
+
+    return novo;
+}
+
+struct racional sorteia_r(int max)
+{
+    struct racional r;
+    int denominador, numerador;
+
+    numerador = aleat(max * -1, max);
+    denominador = aleat(max * -1, max);
+
+    r = cria_r(numerador, denominador);
+    r.valido = valido_r(r);
+    return r;
+}
+
+int numerador_r(struct racional r)
+{
+    return r.num;
+}
+
+int denominador_r(struct racional r)
+{
+    return r.den;
+}
+
+int valido_r(struct racional r)
+{
+    if (!r.den)
+        return 0;
+    return 1;
+}
+
+void imprime_r(struct racional r)
+{
+    if (!simplifica_r(&r))
+        exit(0);
+
+    if (!valido_r(r))
+    {
+        printf("INVALIDO ");
+        return;
+    }
+    else if (r.den == r.num)
+    {
+        printf("1 ");
+        return;
+    }
+
+    else if (!r.num)
+    {
+        printf("0 ");
+        return;
+    }
+    else if (r.den == 1)
+    {
+        printf("%d ", r.num);
+        return;
+    }
+    else
+        printf("%d/%d ", r.num, r.den);
+}
+
+int compara_r(struct racional r1, struct racional r2)
+{
+    float n1, n2;
+    if (valido_r(r1) && valido_r(r2))
+    {
+
+        n1 = numerador_r(r1) / denominador_r(r1);
+        n2 = numerador_r(r2) / denominador_r(r2);
+
+        if (n1 < n2)
+            return -1;
+        else if (n1 > n2)
+            return 1;
+
+        return 0;
+    }
+}
+
+int simplifica_r(struct racional *r)
+{
+    if (valido_r(*r))
+    {
+        int div_comum;
+        div_comum = mdc(r->num, r->den);
+        r->num /= div_comum;
+        r->den /= div_comum;
+
+        if (r->den < 0)
+        {
+            r->num *= -1;
+            r->den *= -1;
+        }
+        return 1;
+    }
+
+    return 0;
+}
+
+int soma_r(struct racional r1, struct racional r2, struct racional *r3)
+{
+    if (valido_r(r1) && valido_r((r2)))
+    {
+        if (denominador_r(r1) == denominador_r(r2))
+        {
+            r3->num = numerador_r(r1) + numerador_r(r2);
+        }
+        else
+        {
+            r3->den = mmc(denominador_r(r1), denominador_r(r2));
+            /*separei em duas linhas a mesma conta*/
+            r3->num = (r3->den / denominador_r(r1)) * numerador_r(r1);
+            r3->num += (r3->den / denominador_r(r2)) * numerador_r(r2);
+        }
+
+        return 1;
+    }
+    return 0;
+}
+
+int subtrai_r(struct racional r1, struct racional r2, struct racional *r3)
+{
+    if (valido_r(r1) && valido_r((r2)))
+    {
+        if (denominador_r(r1) == denominador_r(r2))
+        {
+            /*so muda o sinal*/
+            r3->num = numerador_r(r1) - numerador_r(r2);
+        }
+        else
+        {
+            r3->den = mmc(denominador_r(r1), denominador_r(r2));
+            r3->num = (r3->den / denominador_r(r1)) * numerador_r(r1);
+            r3->num -= (r3->den / denominador_r(r2)) * numerador_r(r2);
+        }
+
+        return 1;
+    }
+    return 0;
+}
+
+int multiplica_r(struct racional r1, struct racional r2, struct racional *r3)
+{
+    if (valido_r(r1) && valido_r((r2)))
+    {
+        r3->num = numerador_r(r1) * numerador_r(r2);
+        r3->den = denominador_r(r1) * numerador_r(r2);
+        return 1;
+    }
+    return 0;
+}
+void troca_var(struct racional *r)
+{
+    int mutz;
+    mutz = denominador_r(*r);
+    r->den = numerador_r(*r);
+    r->num = mutz;
+}
+
+int divide_r(struct racional r1, struct racional r2, struct racional *r3)
+{
+    if (valido_r(r1) && valido_r((r2)))
+    {
+        troca_var(&r2);
+        if (!valido_r(r2))
+            return 0;
+
+        r3 = multiplica_r(r1, r2, **r3);
+        return 1;
+    }
+    return 0;
+}
--- tp2/tp2.c
+++ tp2/tp2.c
@@ -1,11 +1,65 @@
-/* Arquivo MAIN que usa o TAD racionais */
+#include <stdio.h>
+#include <stdlib.h>
+#include "racionais.h"
+#define SEED 0
+#define MAX 100
+void trocar(struct racional *a, struct racional *b);
+void bolha_ord(struct racional r[], int n);
+void imprime_vet(struct racional r[], int n);
 
-/* coloque seus includes aqui */
-
-int main ()
+int main()
 {
+    srand(SEED);
+    int n;
+    struct racional r[MAX];
 
-    /* coloque seu código aqui */
+    do
+    {
+        scanf("%d", &n);
+    } while (n <= 0 || n > 100);
+
+    for (int i = 0; i < n; i++)
+    {
+        r[i] = sorteia_r(n);
+        imprime_r(r[i]);
+    }
+    printf("\n");
+    imprime_vet(r, n);
+
+    bolha_ord(r, n); // ordena
+
+    printf("\n");
+    imprime_vet(r, n);
 
     return 0;
+}
+void imprime_vet(struct racional r[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (valido_r(r[i]))
+            imprime_r(r[i]);
+    }
+}
+
+void bolha_ord(struct racional r[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Últimos i elementos já estão no lugar certo
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            // Troca se o elemento atual for maior que o próximo elemento
+            if (compara_r(r[i], r[i + 1]) == 1)
+            {
+                trocar(&r[j], &r[j + 1]);
+            }
+        }
+    }
+}
+void trocar(struct racional *a, struct racional *b)
+{
+    struct racional mutz = *a;
+    *a = *b;
+    *b = mutz;
 }
