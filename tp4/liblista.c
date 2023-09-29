b869feccc00b4275c3e17caffeaba89225ef20df
camutze <camutze@gmail.com>
2023-09-29 09:22:58 -0300

    comecei o tp4

---
+++ tp4/liblista.c
@@ -0,0 +1,11 @@
+// TAD lista de números inteiros - implementação
+// Autor: 
+
+// Implementação com lista encadeada dupla não-circular
+
+// Coloque os includes necessários a este arquivo aqui.
+
+// Eventuais funções auxiliares (usadas somente neste arquivo) podem vir aqui.
+
+// Implemente as funções definidas em liblista.h aqui.
+
---
+++ tp4/tp4.c
@@ -0,0 +1,37 @@
+// TAD lista de números inteiros - programa de teste
+// Carlos Maziero - DINF/UFPR, Set 2023
+
+// Coloque os includes necessários a este arquivo aqui.
+
+// Eventuais funções auxiliares (usadas somente neste arquivo) podem vir aqui.
+
+int main ()
+{
+  // cria uma lista L vazia
+  // imprime L
+  // insere no final de L os valores 10, 11, 12, 13 e 14, nessa ordem
+  // imprime L
+  // insere no início de L o valor 32
+  // insere no início de L o valor 64
+  // imprime L
+  // insere no meio de L o valor 103
+  // insere no meio de L 0 valor 47
+  // imprime L
+  // para as posições início, 5, última e 100:
+  //   imprime o conteúdo da posição em L
+  // fim para
+  // para os valores 5, 10 e 14:
+  //   imprime posição do valor em L
+  // fim para
+  // para os valores 12, 103 e 79:
+  //   retira de L o elemento com o valor indicado, se estiver em L
+  // fim para
+  // imprime L
+  // enquanto L não estiver vazia
+  //   retira o primeiro valor de L e o imprime
+  //   retira o último valor de L e o imprime
+  // imprime L
+  // fim enquanto
+  // destrói L
+  // encerra
+}
