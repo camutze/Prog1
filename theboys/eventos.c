dc7da254de378d3596d6d1c353de1cdd4361dda6
Carlos <camutze@gamil.com>
2023-11-01 10:45:44 -0300

    Criando eventos

--- theboys/eventos.c
+++ theboys/eventos.c
@@ -95,57 +95,70 @@ void evento_avisa(int tempo, struct base_b *base)
 {
     int elem;
     while (base->presentes->size < base->lotacao && !lista_vazia(base->lista_espera))
-    {    int destino_base_id;
+    {
+        int destino_base_id;
 
+        /*ENTRA (T, H, B):
 
-/*ENTRA (T, H, B):
+        calcula TPB = tempo de permanência na base:
+            TPB = 15 + paciência de H * aleatório [1...20]
+        cria evento SAI (agora + TPB, H, B)*/
+        void evento_entra(struct mundo_m * m, int tempo, int id_heroi, struct base_b *base)
+        {
+            int tpb;
+            tpb = 15 + m->herois[id_heroi]->paciencia * gera_aleat(1, 20);
+            evento_sai(tempo + tpb, id_heroi, base); // arruma isso aqui ó doido
+        }
 
-calcula TPB = tempo de permanência na base:
-    TPB = 15 + paciência de H * aleatório [1...20]
-cria evento SAI (agora + TPB, H, B)*/
-void evento_entra(struct mundo_m *m, int tempo, int id_heroi, struct base_b *base)
-{
-    int tpb;
-    tpb = 15 + m->herois[id_heroi]->paciencia * gera_aleat(1, 20);
-    evento_sai(tempo + tpb, id_heroi, base); // arruma isso aqui ó doido
-}
+        /*O herói H sai da base B. Ao sair, escolhe uma base de destino para viajar; o porteiro de B é avisado, pois uma vaga foi liberada:*/
+        &mundo->bases[id_base_atual]->local, &base->local
+                                                  evento_avisa(tempo, base);
 
-/*O herói H sai da base B. Ao sair, escolhe uma base de destino para viajar; o porteiro de B é avisado, pois uma vaga foi liberada:*/
-&mundo->bases[id_base_atual]->local, &base->local
-    evento_avisa(tempo, base);
+        envento_viaja(tempo, id_heroi, mundo->bases[destino_base_id]);
+    }
+    /*VIAJA (T, H, D):
 
-    envento_viaja(tempo, id_heroi, mundo->bases[destino_base_id]);
-}
-/*VIAJA (T, H, D):
+    calcula duração da viagem:
+        distância = distância cartesiana entre a base atual de H e a base D
+        duração = distância / velocidade de H
+    cria evento CHEGA (agora + duração, H, D)*/
+    void evento_viaja(struct mundo_m * mundo, int tempo, int id_heroi, struct base_b *base)
+    {
+        int id_base_atual;
+        id_base_atual = mundo->herois[id_heroi]->base_id;
 
-calcula duração da viagem:
-    distância = distância cartesiana entre a base atual de H e a base D
-    duração = distância / velocidade de H
-cria evento CHEGA (agora + duração, H, D)*/
-void evento_viaja(struct mundo_m *mundo, int tempo, int id_heroi, struct base_b *base)
-{
-    int id_base_atual;
-    id_base_atual = mundo->herois[id_heroi]->base_id;
+        int distancia;
+        distancia = calcula_distancia(mundo->bases[id_base_atual]->local, base->local);
+        int duracao;
+        duracao = distancia / mundo->herois[id_heroi]->velocidade;
+        evento_chega(tempo + duracao, id_heroi, base->id);
+    }
+
+    /*Evento MISSAO
+    Uma missão M é disparada no instante T. São características de uma missão:
+
+    Cada missão ocorre em um local aleatório e requer um conjunto aleatório de habilidades; ambos são definidos durante a inicialização.
+    Cada equipe é formada pelo conjunto de heróis presentes em uma base.
+    Uma equipe está apta para a missão se a união das habilidades de seus heróis contém as habilidades requeridas pela missão.
+    Deve ser escolhida para a missão a equipe da base mais próxima ao local da missão e que esteja apta para ela.
+    Ao completar uma missão, os heróis da equipe escolhida ganham pontos de experiência.
+    Se uma missão não puder ser completada, ela é marcada como “impossível” e adiada de 24 horas.*/
+
+    void evento_missao(struct mundo_m * mundo, int tempo, struct heroi_h *heroi, struct base_b *base)
+    {
+        // primeira distancia
+        int menor_distancia = calcula_distancia(mundo->missoes[missao_pos]->local, mundo->bases[0]->local);
+        for (int i = 1; i < mundo->n_bases; i++)
+        {
+            
+            distancia = calcula_distancia(mundo->missoes[missao_pos]->local, mundo->bases[i]->local);
+            if(distancia < menor_distancia)
+            {
+                menor_distancia = distancia;
+                base_pos = i;
+            }
+        }
+        /*PERGUNTAR*/
+    }
 
     
-    int distancia;
-    distancia = calcula_distancia(mundo->bases[id_base_atual]->local, base->local);
-    int duracao;
-    duracao = distancia / mundo->herois[id_heroi]->velocidade;
-    evento_chega(tempo + duracao, id_heroi, base->id);
-}
-
-/*Evento MISSAO
-Uma missão M é disparada no instante T. São características de uma missão:
-
-Cada missão ocorre em um local aleatório e requer um conjunto aleatório de habilidades; ambos são definidos durante a inicialização.
-Cada equipe é formada pelo conjunto de heróis presentes em uma base.
-Uma equipe está apta para a missão se a união das habilidades de seus heróis contém as habilidades requeridas pela missão.
-Deve ser escolhida para a missão a equipe da base mais próxima ao local da missão e que esteja apta para ela.
-Ao completar uma missão, os heróis da equipe escolhida ganham pontos de experiência.
-Se uma missão não puder ser completada, ela é marcada como “impossível” e adiada de 24 horas.*/
-
-void evento_missao(struct mundo_m *mundo, int tempo, struct heroi_h *heroi, struct base_b *base)
-{
-    
-}
