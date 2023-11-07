9398e842941beed77eb3b66dd5552341ab834489
camutze <carlosmutzenberg@ufpr.br>
2023-11-07 16:22:53 -0300

    fizpeloalias

--- theboys/simulador.c
+++ theboys/simulador.c
@@ -106,8 +106,11 @@ int cria_missoes(struct mundo_t *mundo)
         missao[i].local.y = gera_aleat(0, N_TAMANHO_MUNDO - 1);
         missao[i].tentativa = 0;
 
-        tam = gera_aleat(6, 10);
-        missao[i].habil = set_create(tam);
+
+        missao[i].habil = set_create(N_HABILIDADES -1);
+        tam = gera_aleat(5, 9);
+        printf("%d \n", tam);
+        
         set_aleat(missao[i].habil, tam);
     }
     mundo->missao = missao;
